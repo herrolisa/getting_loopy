a41f8af7a2698506171b457e5601ae3047fb2245
Lisa Zhou <lisazhou@users.noreply.github.com>
2016-03-08 20:19:48 -1000

    completed presidents

---
+++ loop.js
@@ -0,0 +1,6 @@
+// Presidents - Accessing values in an Array
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+for (var i = 0; i < presidents.length; i++) {
+	console.log("The value if i is: " + i);
+	console.log("The value at Index is: " + presidents[i]);
+}
