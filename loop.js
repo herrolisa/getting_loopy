bd7658531f7dc2008fa3c3db8d22c410451e864b
Lisa Zhou <lisazhou@users.noreply.github.com>
2016-03-08 21:08:14 -1000

    the string of numbers

--- loop.js
+++ loop.js
@@ -4,3 +4,19 @@ for (var i = 0; i < presidents.length; i++) {
 	console.log("The value if i is: " + i);
 	console.log("The value at Index is: " + presidents[i]);
 }
+
+function printContent(arr,arrValue){
+	for (var i = 0; i < arr.length; i++) {
+	arrValue = arr[i];
+	console.log("The value if i is: " + i);
+	console.log("The value at Index is: " + arrValue);
+}
+}
+
+// The String of Numbers
+var	stringOfNumbers = "";
+for (var i = 10; i < 21; i++) {
+      var value = i.toString();
+      stringOfNumbers += value;
+	}
+console.log(stringOfNumbers);
